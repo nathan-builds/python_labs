1590dd1d735ed6a32154825bc12f63dc27c1f8f4
nathan-builds <61122637+nathan-builds@users.noreply.github.com>
2020-02-28 17:25:30 -0500

    Add files via upload

---
+++ count_distinct_sums_and_products.py
@@ -0,0 +1,20 @@
+def count_distinct_sums_and_products(items):
+    seen_numbers = []
+    if len(items) == 1:
+        return 2
+    elif not items:
+        return 0
+    else:
+
+        for number in items:
+            for num in items:
+                addition = number + num
+                multiplication = number * num
+                if addition not in seen_numbers:
+                    seen_numbers.append(addition)
+                if multiplication not in seen_numbers:
+                    seen_numbers.append(multiplication)
+
+    return len(seen_numbers)
+
+# Not sure if this will pass all the tests for example try [1,2]
