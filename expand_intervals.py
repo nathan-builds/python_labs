956b80c1d9bd00c2197ebcedfead3505e16f5444
nathan-builds <61122637+nathan-builds@users.noreply.github.com>
2020-03-19 14:23:12 -0400

    Update expand_intervals.py

--- expand_intervals.py
+++ expand_intervals.py
@@ -1,17 +1,27 @@
+def expand_intervals(intervals):
+
+    new_intervals = intervals.split(',')
+
+    working_list = []
+    answer = []
+
+    for pair in new_intervals:
+        temp = pair.split('-')
+        working_list.append(temp)
+
+    for number in working_list:
+        if len(number) > 1:
+            start = int(number[0])
+            end = int(number[1])
+            for x in range(start, end+1):
+                answer.append(x)
+        else:
+            answer.append(int(number[0]))
+
+    return answer
 
 
 
-numbers='4-6,10-12,16'
-
-check_list=numbers.replace('-',',')
-final_list=[]
-
-my_list=numbers.split(",")
-
-for pair in numbers:
-    current_numbers=pair.replace('-',',')
-    print(current_numbers[0])
-    #for x in range(int(current_numbers[])
 
 
 
